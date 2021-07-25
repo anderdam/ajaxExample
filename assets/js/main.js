24970b3e0cc9082db6317f533ee5112f3f03b12e
AndersonD <anderdam@gmail.com>
2021-07-24 22:08:45 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+document.addEventListener('click', e => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if (tag === 'a') {
+        e.preventDefault();
+        carregaPagina(el);
+    }
+});
+
+async function carregaPagina(el) {
+    try {
+        
+    const href = el.getAttribute('href');
+    const response = await fetch(href);
+    
+    if (response.status !== 200) throw new Error(response.statusText);
+
+    const html = await response.text();
+    carregaResultado(html);
+
+    } catch (error) {
+        console.log(error);
+    }    
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = response;
+}
+    
